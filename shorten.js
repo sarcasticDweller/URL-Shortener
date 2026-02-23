c8a57cb9b17fac44a46c72178d9bf28830cd9b2a
sarcasticDweller <47394104+sarcasticDweller@users.noreply.github.com>
2026-02-23 19:54:06 +0000

    detect urls maybe

--- shorten.js
+++ shorten.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const urlSnippet = "https://www.amazon.com";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "OFF",
+    })
+});
+
+chrome.action.onClickced.addListener(async (tab) => {
+    if (tab.url.startsWith(urlSnippet)) {
+        alert("amazon detected!");
+    }
+})
