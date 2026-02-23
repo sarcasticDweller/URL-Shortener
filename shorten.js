3135496c6f813e7828fb347ff3708c6728a54f53
sarcasticDweller <47394104+sarcasticDweller@users.noreply.github.com>
2026-02-23 20:57:25 +0000

    let me download youuu

--- shorten.js
+++ shorten.js
@@ -7,8 +7,28 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 
-chrome.action.onClickced.addListener(async (tab) => {
-    if (tab.url.startsWith(urlSnippet)) {
-        alert("amazon detected!");
+/*
+async function getCurrentTabUrl() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    if (tab.url && tab.url.startsWith(urlSnippet)) {
+        return tab.url;
+    } else {
+        return null;
     }
-})
+}
+*/
+
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && changeInfo.url.startsWith(urlSnippet)) {
+        chrome.action.setBadgeText({
+            text: "ON",
+        });
+    } else {
+        chrome.action.setBadgeText({
+            text: "OFF",
+        });
+    }
+});
